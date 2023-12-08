5254ebe54988f5e6a2f83515a853223d4fbcc0b7
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2023-12-08 17:46:37 -0300

    adicionando atividades da aula 3

---
+++ atividade05.java
@@ -0,0 +1,21 @@
+package atividadesaula03;
+
+import java.util.Scanner;
+
+public class atividade05 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		int n, somaPositivos=0;
+
+		do {
+			System.out.println("Digite o número: ");
+			n = leia.nextInt();
+			if (n >0) {
+				somaPositivos = somaPositivos + n; 
+			}
+		} while (n != 0);
+		
+		System.out.println("A soma dos números positivos é: "+somaPositivos);
+	}
+}
---
+++ atividade06.java
@@ -0,0 +1,28 @@
+package atividadesaula03;
+
+import java.util.Scanner;
+
+public class atividade06 {
+
+	public static void main(String[] args) {
+		
+			Scanner leia = new Scanner(System.in);
+			int n, somaMultiplos3=0,contSomaMultiplos3=0, mediaDeMultiplos;
+
+
+			do {
+				System.out.println("Digite o número: ");
+				n = leia.nextInt();
+				if (n>0 && n%3==0) {
+					somaMultiplos3 = somaMultiplos3 + n; 
+					contSomaMultiplos3++;
+				}
+			} while (n != 0);
+			
+			 mediaDeMultiplos = somaMultiplos3/contSomaMultiplos3;
+			System.out.println("A média dos números multiplos de 3 é: "+ mediaDeMultiplos);
+		}
+
+	}
+
+
